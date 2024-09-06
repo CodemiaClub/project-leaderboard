b04c4baff262242f3c1cd8f5d7b68d95a2cfb4b9
Ale2301 <alejo.2001@live.com.ar>
2024-09-06 14:30:03 -0300

    feat: better handle for errors

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import express, { raw } from "express";
-import { google } from 'googleapis';
-import * as path from 'path';
-import * as fs from 'fs';
+import { google } from "googleapis";
+import * as path from "path";
+import * as fs from "fs";
 
 const app = express();
 
@@ -16,50 +16,40 @@ if (!process.env.SPREADSHEETID) {
 }
 const spreadsheetID = process.env.SPREADSHEETID;
 
-const keyFilePath = path.join(__dirname,"../keys.json")
+const keyFilePath = path.join(__dirname, "../keys.json");
 
-
-async function accessSheet(){
-  try{
-    const auth = new google.auth.GoogleAuth({
-      keyFile:keyFilePath,
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    })
-    const sheets = google.sheets({version:"v4",auth})
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId:spreadsheetID,
-      range:"Totales"
-    })
-    return response.data.values
-  }catch(e){
-    console.error("error: " + e)
-  }
+async function accessSheet() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: keyFilePath,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  const sheets = google.sheets({ version: "v4", auth });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: spreadsheetID,
+    range: "Totales",
+  });
+  return response.data.values;
 }
 
 async function getData() {
-  try {
-    const rawData = await accessSheet();
-    const header = rawData![0]
-    const rows = rawData!.slice(1)
-    const formattedData = rows.map((row) => {
-      return Object.fromEntries(
-        header.map((key, index) => [key, row[index]])
-      );
-    });
+  const rawData = await accessSheet();
+  const header = rawData![0];
+  const rows = rawData!.slice(1);
+  const formattedData = rows.map((row) => {
+    return Object.fromEntries(header.map((key, index) => [key, row[index]]));
+  });
 
-    return formattedData;
-  } catch (e) {
-    console.error("Error formatting data: " + e);
-    throw e;
-  }
+  return formattedData;
 }
 
 app.get("/", async (req, res) => {
   try {
-    const data = await getData()
-    res.json(data)
+    const data = await getData();
+    console.log(data);
+    res.status(200).json(data);
   } catch (error) {
-    res.status(500).send("Error")
+    console.error(error);
+    res.status(500).send(error);
   }
 });
 
