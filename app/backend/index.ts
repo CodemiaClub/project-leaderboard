caf4ec693eadbcf961791d0356aa656f6ad13ffb
Ale2301 <alejo.2001@live.com.ar>
2024-09-06 14:49:35 -0300

    fix: removed unused fs

--- app/backend/index.ts
+++ app/backend/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express, { raw } from "express";
 import { google } from "googleapis";
 import * as path from "path";
-import * as fs from "fs";
+
 
 const app = express();
 
