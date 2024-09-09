d0f49008aa9db3288a454aab2bf5bf28f6a7b7e2
ariix00 <armandoferra2685@gmail.com>
2024-09-09 16:34:37 -0300

    App and Footer added

--- app/frontend/src/App.tsx
+++
@@ -1,3 +0,0 @@
-const App = () => <div className="bg-slate-900 min-w-screen min-h-screen"></div>;
-
-export { App };
---
+++ app/frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+import logo from "../assets/logo.png";
+
+const Footer = () => <footer className="bg-slate-700 flex items-center fixed bottom-0 left-0 w-full">
+    <div className="container mx-auto flex-row relative">
+    <p className="text-center text-zinc-100 text-sm font-sans pt-6 pb-6">Este sitio fue diseñado y desarrollado exclusivamente por estudiantes de Codemia bajo <br /> supervisión técnica</p>
+    </div>
+    <div className='h-24 w-24 absolute right-1 bottom-0'>
+        <img className='h-full w-full p-4' src={logo} alt=""/>
+    </div>
+</footer>
+
+export default Footer;
