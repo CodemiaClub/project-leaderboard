1ba8bbb2141282167cec05da7a19aeb54dd9e7d9
ariix00 <armandoferra2685@gmail.com>
2024-09-09 20:32:27 -0300

    changes added

--- app/frontend/src/components/App.tsx
+++ app/frontend/src/components/App.tsx
@@ -1,21 +1,14 @@
 import Footer from "./Footer";
 
-export const App = () => { 
-return(
-<>
-<div className="bg-slate-900 min-w-screen min-h-screen">
-    <main className="flex justify-center items-center h-screen">
-     
-    <section className="bg-slate-300 min-w-8/12 sm:w-2/4 md:w-2/4 h-3/4 flex-col mb-20 rounded-lg p-4">
-  
-    </section>
-
-    </main>
-    <Footer> 
-    </Footer>
-</div>
-
-</>
-
-)}
-
+export const App = () => {
+  return (
+    <>
+      <div className="bg-slate-900 min-w-screen min-h-screen">
+        <main className="flex justify-center items-center h-screen">
+          <section className="bg-slate-300 min-w-8/12 sm:w-2/4 md:w-2/4 h-3/4 flex-col mb-20 rounded-lg p-4"></section>
+        </main>
+        <Footer></Footer>
+      </div>
+    </>
+  );
+};
