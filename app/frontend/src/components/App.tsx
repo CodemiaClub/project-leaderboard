0f9a4fd77fd87d4c680879097f5b90ff600b771b
Graci <gracipm92@gmail.com>
2024-09-11 10:42:21 -0300

    Add CardContent component and format code with Prettier

--- app/frontend/src/components/App.tsx
+++ app/frontend/src/components/App.tsx
@@ -1,13 +1,16 @@
 import Footer from "./Footer";
+import CardContent from "./CardContent";
 
 export const App = () => {
   return (
     <>
       <div className="bg-slate-900 min-w-screen min-h-screen">
         <main className="flex justify-center items-center h-screen">
-          <section className="bg-slate-300 min-w-8/12 sm:w-2/4 md:w-2/4 h-3/4 flex-col mb-20 rounded-lg p-4"></section>
+          <section className="bg-slate-300 min-w-8/12 sm:w-2/4 md:w-2/4 h-3/4 flex-col mb-20 rounded-lg p-4">
+            <CardContent />
+          </section>
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
