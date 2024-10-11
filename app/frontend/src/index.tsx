7f8c1b63471784f58840c1e403567d6ab48b1746
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-10-11 10:00:39 -0300

    feat: Added environment variable and error handling

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 
+if (!process.env.NODEJS_URI) {
+  throw new Error(
+    "No NODEJS_URI environment variable found. Please refer to README.md for more info.",
+  );
+}
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(<App />);
