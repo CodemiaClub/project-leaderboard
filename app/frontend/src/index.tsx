fe564771b49c6803f286219b5d5927a70f0e31b8
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-10-14 09:26:39 -0300

    refactor: Removed unrelated changes from this PR

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,12 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 
-if (!process.env.URL) {
-  throw new Error(
-    "No URL environment variable found. Please refer to README.md for more info.",
-  );
-}
-
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(<App />);
