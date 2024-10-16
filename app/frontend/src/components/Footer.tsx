80faa95878efa5be85d67b010660e613cc0ff2fb
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-10-16 17:18:13 -0300

    add desktop footer size

--- app/frontend/src/components/Footer.tsx
+++ app/frontend/src/components/Footer.tsx
@@ -3,14 +3,18 @@ import logo from "../assets/logo.png";
 const Footer = () => (
   <footer className="bg-Codemia-green-dark-2 w-full px-3 py-5">
     <div className="md:w-2/4 flex flex-col md:flex-row items-center justify-center mx-auto">
-      <div className="container mx-auto box-border order-last md:order-first">
-        <p className="text-center text-zinc-100 text-sm font-sans font-lato px-5 md:pl-0 md:pr-3  pt-5 md:pt-0">
+      <div className="box-border order-last md:order-first">
+        <p className="text-center text-zinc-100 text-sm font-sans font-lato px-5 md:pl-0 md:pr-12 pt-5 md:pt-0">
           Este sitio fue diseñado y desarrollado exclusivamente por estudiantes
           de Codemia bajo supervisión técnica.
         </p>
       </div>
       <div className="h-[50px]">
-        <img className="aspect-square w-[50px] h-full" src={logo} alt="" />
+        <img
+          className="aspect-square w-[50px] h-full"
+          src={logo}
+          alt="Logo de Codemia"
+        />
       </div>
     </div>
   </footer>
