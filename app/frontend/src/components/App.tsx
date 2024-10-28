e6d81b8d5d66d0a9230e810300394f89735e9462
ariix00 <armandoferra2685@gmail.com>
2024-10-28 18:29:21 -0300

    scroll fixed

--- app/frontend/src/components/App.tsx
+++ app/frontend/src/components/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
     <CoinsProvider>
       <div className="bg-Codemia-bg min-w-screen min-h-dvh bg-no-repeat bg-fixed bg-cover grid grid-rows-[1fr,auto]">
         <main className="flex justify-center my-10">
-          <section className="bg-Codemia-green2/[.7] mx-1 md:mx-0 min-w-8/12 sm:w-2/4 md:w-2/4 min-h-[600px] flex-col rounded-3xl p-2 md:p-4 backdrop-blur-sm shadow-Codemia-dark-ultra shadow-xl">
+          <section className="bg-Codemia-green2/[.7] mx-1 md:mx-0 min-w-8/12 max-h-[800px] sm:w-2/4 md:w-2/4 min-h-[600px] flex-col rounded-3xl p-2 md:p-4 backdrop-blur-sm shadow-Codemia-dark-ultra shadow-xl">
             <CardContent />
           </section>
         </main>
